82ae23ba37e48e3117e4197add21f1adf450716f
Thalles <thalles-brandao@hotmail.com>
2019-10-01 12:56:44 -0300

    Added movie Hellraiser (1987)

---
+++ movies/1987/hellraiser.js
@@ -0,0 +1,28 @@
+{
+    "name": "Hellraiser",
+    "year": 1987,
+    "runtime": 93,
+    "categories": [
+        "horror",
+        "fantasy"
+    ],
+    "release-date": "1987-09-10",
+    "director": "Clive Barker",
+    "writer": "Clive Barker",
+    "actors": [
+        "Doug Bradley",
+        "Sean Chapman",
+        "Oliver Smith",
+        "Ashley Laurence",
+        "Andrew Robinson",
+        "Clare Higgins",
+        "Robert Hines",
+        "Frank Baker",
+        "Kenneth Nelson",
+        "Nicholas Vince",
+        "Simon Bamford",
+        "Grace Kirby"
+    ],
+    "storyline": "The film involves the resurrection of Frank (Sean Chapman), who had opened the door to an alternate dimension and had his body torn to pieces by creatures known as Cenobites. Years later, Frank's brother Larry (Andrew Robinson) moves into their late mother's abandoned house with new wife Julia (Clare Higgins)"
+}
+  
